d9c092e364ee5e81b1718abef00ee50823e71f99
Mykhailo Danylov <mike.danylov@gmail.com>
2016-01-30 17:02:03 +0200

    Congigured app with ngRoute

--- AngularJS/Single Page Applications/app/scripts/app.js
+++ AngularJS/Single Page Applications/app/scripts/app.js
@@ -1,3 +1,27 @@
 'use strict';
 
-angular.module('confusionApp', []);
+angular.module('confusionApp', ['ngRoute'])
+.config(function ($routeProvider) {
+	
+	$routeProvider
+
+	//route to contacts page
+	.when('/contactus', {
+		templateUrl: 'contactus.html',
+		controller: 'ContactController'
+	})
+
+	// route to menu page
+	.when('/menu', {
+		templateUrl: 'menu.html',
+		controller: 'MenuController'
+	})
+
+	// route to dish details page
+	.when('/menu/:id', {
+		templateUrl: 'dishdetails.html',
+		controller: 'DishDetailsController'
+	})
+
+	.otherwise({ redirectTo: '/contactus' });
+});
--- AngularJS/Single Page Applications/app/scripts/controllers.js
+++ AngularJS/Single Page Applications/app/scripts/controllers.js
@@ -67,9 +67,9 @@ angular.module('confusionApp')
 	};
 }])
 
-.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
-	
-	$scope.dish= menuFactory.getDish(3);
+.controller('DishDetailsController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
+	var dish = menuFactory.getDish(parseInt($routeParams.id, 10));
+	$scope.dish = dish;
 }])
 
 .controller('DishCommentController', ['$scope', function($scope) {
